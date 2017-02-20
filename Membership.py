20ab850bb28a758ceb9c8c42aaf4082566e60bb4
bajabera <bajabera@uni.coventry.ac.uk>
2017-02-20 21:30:01 +0000

    Update Membership.py

--- Membership.py
+++ Membership.py
@@ -1,15 +1,33 @@
 def membership():
     '''
-    Functio to determine membership
+    Function to register the user as a member
+    Takes in user input to determine which membership to register the user under
+
     '''
+    
     user_membership=None #User has no membership yet
     
     try:
         if user_status=='new user':#if the user is a newly registered user
             registration()#first register the user before becoming a member
+            
         elif user_status=='existing user':
-            ask=input('Would you like to join our basic membership program or our Loyalty membership program?' )#Asks which membership program they would like to join
+            ask=input('Would you like to join our basic membership program or our Loyalty membership program? ' )#Asks which membership program they would like to join
 
             if ask=='Basic':
-                user_membership=Basic
+                user_membership='Basic Member'
+                print('You will have to pay a one off fee')
+                payment()#a one off payment is required to become a basic member
                 
+                if booked_sessions>=10:#Member can upgrade their membership after booking 10 sessions
+                    upgrade=input('Would you like to upgrade to the loyalty membership? )
+
+                                  
+            elif ask=='Loyal' or 'Loyalty':
+                user_membership='Loyalty Member'
+                print('You will have to pay an annual fee')
+                
+    except TypeError:
+        if not ask.isalpha():
+		print('Only use letters to type in your membership preference')
+	

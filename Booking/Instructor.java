9dbe9766e35f0fb44e70b9c59455e1122519a265
VladPavel15 <vitanvlad@yahoo.com>
2017-03-09 10:19:21 +0000

    Update Instructor.java

--- Booking/Instructor.java
+++ Booking/Instructor.java
@@ -1,7 +1,7 @@
 public class Instructor {
-	public Object firstName;
-	public Object lastName;
-	public Object employeeID;
+	private string firstName;
+	private string lastName;
+	private int employeeID;
 	public void getEmployeeID() {
 	
 	}

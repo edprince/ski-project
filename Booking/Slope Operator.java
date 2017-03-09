70ada57fc8803e39b343ecd08b57a80ea84c8227
VladPavel15 <vitanvlad@yahoo.com>
2017-03-09 10:17:20 +0000

    Add files via upload

---
+++ Booking/Slope Operator.java
@@ -0,0 +1,16 @@
+public class Slope Operator {
+	public Object firstName;
+	public Object lastName;
+	public Object employeeID;
+	public void createSession() {
+	
+	}
+	
+	public void recivePayment() {
+	
+	}
+	
+	public void getEmployeeID() {
+	
+	}
+}

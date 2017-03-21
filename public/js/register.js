1c411631c1d0bc00355bbc4d2d1bfbbaf82e610f
Edward Prince <princee3@coventry.ac.uk>
2017-03-21 15:45:45 +0000

    register.js as classes

--- public/js/register.js
+++ public/js/register.js
@@ -1,16 +1,18 @@
 //Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBw2hHMudDYVgfhsWMr6j2fMpOZ8RhZOKw",
-    authDomain: "sphere-c41ce.firebaseapp.com",
-    databaseURL: "https://sphere-c41ce.firebaseio.com", storageBucket: "sphere-c41ce.appspot.com", messagingSenderId: "204422136162"
-};
+class DatabaseConnection = {
+  constructor() {
+    var config = {
+        apiKey: "AIzaSyBw2hHMudDYVgfhsWMr6j2fMpOZ8RhZOKw",
+        authDomain: "sphere-c41ce.firebaseapp.com",
+        databaseURL: "https://sphere-c41ce.firebaseio.com", storageBucket: "sphere-c41ce.appspot.com", messagingSenderId: "204422136162"
+    }
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
-//INITIALISE FIREBASE
-firebase.initializeApp(config);
-var database = firebase.database();
-var register_submit = document.getElementById('submit');
+  }
+}
 
-class user = {
+class User = {
   constructor(firstname, surname, email, password, membership) {
     this.firstname = firstname;
     this.surname = surname;
@@ -20,30 +22,53 @@ class user = {
   }
 }
 
-register_submit.addEventListener('click', function() {
-  var newUser = new user(
-    get('user_firstname'),
-    get('user_surname'),
-    get('user_email'),
-    get('user_password'),
-    document.querySelector('input[name="membership"]:checked').value
-  );
-  if (newUser.membership == 'member') {
-    newUser.membership = 1;
-  } else {
-    newUser.membership = 2;
+class UserInterface = {
+  constructor() {
+    var register_submit = document.getElementById('submit');
+    register_submit.addEventListener('click', function() {
+      var newUser = new user(
+        get('user_firstname'),
+        get('user_surname'),
+        get('user_email'),
+        get('user_password'),
+        document.querySelector('input[name="membership"]:checked').value
+      );
+      if (newUser.membership == 'member') {
+        newUser.membership = 1;
+      } else {
+        newUser.membership = 2;
+      }
+      var validate = new Validate();
+      if (validate.email(newUser.email) && validate.name(newUser.firstname, newUser.surname) /*&& validateDob(dob)*/) {
+        //Submit details to databaseURL
+        var db = new ReadWriteDatabase();
+        db.createUser(newUser);
+        db.writeUserInfo(newUser);
+        //writeUserData(email, firstname, surname, dob, membership);
+      } else {
+        //Return user to empty form to re-complete;
+        get('user_email') = '';
+        get('user_firstname') = '';
+        get('user_surname') = '';
+        alert('There was an error with some of your information. Please re-enter.');
+      };
+    });
   }
-  if (validateEmail(newUser.email) && validateName(newUser.firstname, newUser.surname) /*&& validateDob(dob)*/) {
-    //Submit details to database
-    console.log('Creating user');
-    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).catch(function(error) {
+}
+
+class ReadWriteDatabase = {
+  constructor() {
+    console.log('Ready to communicate with database')
+  }
+  function createUser(user) {
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
       alert(errorMessage);
     });
-    console.log('done');
-    uid = firebase.auth().currentUser.uid;
-    console.log(uid);
+  }
+  function writeUserInfo(user) {
+    var uid = firebase.auth().currentUser.uid;
     database.ref('user/' + uid).set({
       'firstname': newUser.firstname,
       'surname': newUser.surname,
@@ -51,34 +76,31 @@ register_submit.addEventListener('click', function() {
       //Tier 1,2,3 (registered, free basic, loyalty)
       'membership_tier': newUser.membership
     });
-
-//writeUserData(email, firstname, surname, dob, membership);
-  } else {
-//Return user to form to re-complete;
-    get('user_email') = '';
-    get('user_firstname') = '';
-    get('user_surname') = '';
-    alert('There was an error with some of your information. Please re-enter.');
-  };
-});
-//Validation Functions
-function validateEmail(email) {
-  //var re = new regExp(".*@.*\..*");
-  //return email.match(re);
-  return true
-};
-
-function validateName(name) {
-  if (name) {
-    return true
   }
-};
+}
 
-function validateDob(dob) {
-  //Validates a user submitted date of birth
-  return true
-};
+class Validate = {
+  constructor() {};
+  //Validation Functions
+  function email(email) {
+    //var re = new regExp(".*@.*\..*");
+    //return email.match(re);
+    return true;
+  };
+  function name(name) {
+    if (name) {
+      return true;
+    }
+  };
+  function dob(dob) {
+    //Validates a user submitted date of birth
+    return true;
+  };
+}
 
 function get(id) {
   return document.getElementById(id).value;
 }
+
+var db_cnx = new DatabaseConnection();
+var ui = new UserInterface();

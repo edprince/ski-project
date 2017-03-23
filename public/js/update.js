1eeea2e0b0c245d694385a18b863589e1afd42fc
Plamen Atanasov <atanaso4@uni.coventry.ac.uk>
2017-03-23 11:06:34 +0000

    Update functionality - classes

--- public/js/update.js
+++ public/js/update.js
@@ -1,41 +1,85 @@
+function get(id) { 
+  return document.getElementById(id).value;
+}
+
 //Initialize Firebase
-var config = {
-    apiKey: "AIzaSyD1__SyPP9HQ4-ViwJxH6s8EACpCY9-k34",
-    authDomain: "skiproject-8236e.firebaseapp.com",
-    databaseURL: "https://skiproject-8236e.firebaseio.com",
-    storageBucket: "skiproject-8236e.appspot.com",
-    messagingSenderId: "633876681860"
-  };
-//INITIALISE FIREBASE
-	firebase.initializeApp(config);
-	var userRef = firebase.database().ref().child('user');
-	var searchUser = document.getElementById("Search");
-	var searchButton = document.getElementById("SearchButton");
-	var deleteUser = document.getElementById("Delete");
-	var updateUser = document.getElementById("Update");
-	searchButton.addEventListener('click', function() {
-	var text = searchUser.value;
-	userRef.orderByChild("firstname").equalTo(text).on("child_added", function(snapshot) {
-	console.log(snapshot.val().firstname +" " + snapshot.val().surname);
-	console.log(snapshot.val());
-	var firstname = document.getElementById("firstname_update");
-	var surname = document.getElementById("surname_update");
-	var email = document.getElementById("email_update");
-	var address = document.getElementById("address_update");
-	var membership = document.getElementById("membership_update");
-	var dob = document.getElementById("dob_update");
-	firstname.value = snapshot.val().firstname;
-	surname.value = snapshot.val().surname;
-	email.value = snapshot.val().email;
-	address.value = snapshot.val().address;
-	membership.value = snapshot.val().membership;
-	dob.value = snapshot.val().dob;
-	deleteUser.addEventListener('click', function() {
-		snapshot.ref.remove();
-		window.alert("Member Deleted!");
-		window.location.reload();
+class DatabaseConnection {
+  constructor() {
+    var config = {
+    	apiKey: "AIzaSyD1__SyPP9HQ4-ViwJxH6s8EACpCY9-k34",
+   	authDomain: "skiproject-8236e.firebaseapp.com",
+    	databaseURL: "https://skiproject-8236e.firebaseio.com",
+    	storageBucket: "skiproject-8236e.appspot.com",
+    	messagingSenderId: "633876681860"
+ }
+    firebase.initializeApp(config);
+    var database = firebase.database();
+
+  }
+}
+
+class Member {
+  constructor(firstname, surname, email, address, dob, password, membership){
+	this.firstname = firstname;
+	this.surname = surname;
+	this.email = email;
+	this.address = address;
+	this.dob = dob;
+	this.password = password;
+	this.membership = membership;
+   }
+}
+
+class SlopeUI {
+  constructor() {
+  var searchButton = document.getElementById("SearchButton");
+  var deleteMember = document.getElementById("Delete");
+  var updateMember = document.getElementById("Update");
+  searchButton.addEventListener('click', function() {
+	var db = SearchMemberController();
+	db.SearchMember(Member);
+	db.DisplayMember(Member);
 	});
-   	updateUser.addEventListener('click', function() {
+  updateMember.addEventListener('click', function() {
+	var db = UpdateMemberController();
+	db.UpdateMember(Member);
+  });
+  deleteMember.addEventListener('click', function() {
+	var db = DeleteMemberController();
+	db.DeleteMember(Member);
+  });  
+   }
+}
+  	
+class SearchMemberController {
+  constructor() {
+     SearchMember(Member){
+	var SearchMember = document.getElementById("Search");
+  	var text = searchMember.value;
+	var memberRef = firebase.database().ref().child('user');
+	memberRef.orderByChild("firstname").equalTo(text).on("child_added", function(snapshot) {
+	console.log(snapshot.val().firstname +" " + snapshot.val().surname);
+   DisplayMember(Member) {
+		var firstname = document.getElementById("firstname_update");
+		var surname = document.getElementById("surname_update");
+		var email = document.getElementById("email_update");
+		var address = document.getElementById("address_update");
+		var membership = document.getElementById("membership_update");
+		var dob = document.getElementById("dob_update");
+		firstname.value = snapshot.val().firstname;
+		surname.value = snapshot.val().surname;
+		email.value = snapshot.val().email;
+		address.value = snapshot.val().address;
+		membership.value = snapshot.val().membership;
+		dob.value = snapshot.val().dob;
+	};
+     });
+   }
+}
+
+class UpdateMemberController = {
+  constructor() {
+    UpdateMember(Member) {
 	var firstname = document.getElementById("firstname_update");
 	var surname = document.getElementById("surname_update");
 	var email = document.getElementById("email_update");
@@ -48,50 +92,27 @@ var config = {
 	var address_update = address.value;
 	var membership_update = membership.value;
 	var dob_update = dob.value;
-	if (validateEmail(email) && validateName(firstname, surname) && validateAddress(address) /*&& validateDob(dob)*/) {
-	 	snapshot.ref.set({
-			'firstname': firstname_update,
-			'surname': surname_update,
-			'email': email_update,
-			'address': address_update,
-			'membership':membership_update,
-			'dob': dob_update
-		});
-		$("#table_body").append("<tr><td>" + firstname_update + "</td><td>" + surname_update + "</td><td>" + address_update + "</td><td>" + email_update + "</td><td>" + membership_update + "</td><td>" + dob_update + "</tr></td>")
-
-    } else {
-//Return user to form to re-complete;
-    email = '';
-    firstname = '';
-    surname = '';
-    dob = '';
-    address = '';
-    alert('There was an error with some of your information. Please re-enter.');
-  };
+	snapshot.ref.set({
+	'firstname': firstname_update,
+	'surname': surname_update,
+	'email': email_update,
+	'address': address_update,
+	'membership':membership_update,
+	'dob': dob_update
 	});
-	});
-	});
+	$("#table_body").append("<tr><td>" + firstname_update + "</td><td>" + surname_update + "</td><td>" + address_update + "</td><td>" + email_update + "</td><td>" + membership_update + "</td><td>" + dob_update + "</tr></td>")
 
-//Validation Functions
-function validateEmail(email) {
-  //var re = new regExp(".*@.*\..*");
-  //return email.match(re);
-  return true
-};
+	}
+   }
+}
 
-function validateName(name) {
-  if (name) {
-    return true
+class DeleteMemberController {
+  constructor() {
+  	DeleteMember(Member) {
+	var uid = firebase.auth().currentUser.uid;
+	firebase.database().ref('user/' + uid).remove();
+	window.alert("Member Deleted");
+	window.location.reload();
+	};
   }
-};
-
-function validateDob(dob) {
-  //Validates a user submitted date of birth
-  return true
-};
-
-function validateAddress(address) {
-  if (address) {
-    return true
-  };
-};
+}

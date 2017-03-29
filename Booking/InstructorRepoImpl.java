3fc5fa0d1a2fc92a00384f1deeb80f853c338aab
VladPavel15 <vitanvlad@yahoo.com>
2017-03-29 16:30:01 +0100

    Update InstructorRepoImpl.java

--- Booking/InstructorRepoImpl.java
+++ Booking/InstructorRepoImpl.java
@@ -1,5 +1,5 @@
 public class InstructorRepoImpl {
 	public ArrayList<Instructor> instructors;
-	public InstructorReopo Unnamed1;
-	public Instructor Unnamed2;
+	public InstructorReopo ;
+	public Instructor ;
 }

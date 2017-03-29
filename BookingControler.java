ab6cdb3a47b58b357e7c2f356afc3e773ad95b30
VladPavel15 <vitanvlad@yahoo.com>
2017-03-29 16:23:33 +0100

    Add files via upload

---
+++ BookingControler.java
@@ -0,0 +1,18 @@
+public class BookingControler {
+	public CustomerRepoImpl; customerRepo;
+	public InstructorRepoImpl; instructorRepo;
+	public Session Unnamed2;
+	public Instructor Unnamed3;
+	public Customer Unnamed4;
+	public void makeSession(int slopeID, Date startingTime, Date endingTime) {
+	
+	}
+	
+	public void registerCustomer(string name, int customerID) {
+	
+	}
+	
+	public void selectInstructor(string employeeName, int employeeId) {
+	
+	}
+}

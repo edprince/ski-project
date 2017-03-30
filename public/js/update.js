b0887cb2fdfea1315fded45e19ec1acbcb1d311e
bajabera <bajabera@uni.coventry.ac.uk>
2017-03-30 12:27:11 +0100

    Update update.js

--- public/js/update.js
+++ public/js/update.js
@@ -1,4 +1,4 @@
-function get(id) {
+unction get(id) {
 	return document.getElementById(id).value;
 }
 
@@ -33,7 +33,6 @@ class Member {
 class SlopeUI {
   constructor() {
     var searchButton = document.getElementById("SearchButton");
-    var deleteMember = document.getElementById("Delete");
     var updateMember = document.getElementById("Update");
     var clearButton = document.getElementById("Clear");	  
     searchButton.addEventListener('click', function() {	    
@@ -44,10 +43,6 @@ class SlopeUI {
       var db = new UpdateMemberController();
       db.UpdateMember(Member);
     });
-    deleteMember.addEventListener('click', function() {
-      var db = new DeleteMemberController();
-      db.DeleteMember(Member);
-    });
     clearButton.addEventListener('click', function() {
       window.location.reload();
     });	    
@@ -55,6 +50,7 @@ class SlopeUI {
 }
 
 var userReference;
+
 class SearchMemberController {
   constructor() {}
     SearchMember(member) {
@@ -64,17 +60,12 @@ class SearchMemberController {
     memberRef.orderByChild("firstname").equalTo(text).on("child_added", function(snapshot) {
     	userReference = snapshot.key; 
     	var firstname = document.getElementById("firstname_update");
-   	var surname = document.getElementById("surname_update");
-    	var email = document.getElementById("email_update");
-    	var address = document.getElementById("address_update");
+        var surname = document.getElementById("surname_update");
     	var membership = document.getElementById("membership_update");
-    	var dob = document.getElementById("dob_update");
     	firstname.value = snapshot.val().firstname;
     	surname.value = snapshot.val().surname;
-    	email.value = snapshot.val().email;
-    	address.value = snapshot.val().address;
     	membership.value = snapshot.val().membership;
-    	dob.value = snapshot.val().dob; 
+    	 
     });
     }
   
@@ -84,42 +75,24 @@ class SearchMemberController {
 class UpdateMemberController {
 	constructor(){}
     UpdateMember() {
-    var firstname = document.getElementById("firstname_update");
-    var surname = document.getElementById("surname_update");
-    var email = document.getElementById("email_update");
-    var address = document.getElementById("address_update");
     var membership = document.getElementById("membership_update");
-    var dob = document.getElementById("dob_update");
-    var firstname_update = firstname.value;
-    var surname_update = surname.value;
-    var email_update = email.value;
-    var address_update = address.value;
     var membership_update = membership.value;
-    var dob_update = dob.value;
     var userRef = firebase.database().ref().child('user').userReference;
     firebase.database().ref('user/' + userReference).set({
-	    	'firstname': firstname_update,
-		'surname': surname_update,
-		'email': email_update,
-		'address': address_update,
 		'membership':membership_update,
-		'dob': dob_update
     });
 	$("#table_body").append("tr.><td>" + firstname_update + "</td><td>" + surname_update + "</td><td>" + address_update + "</td><td>" + email_update + "</td><td>" + membership_update + "</td><td>" + dob_update + "</tr></td>");
   }
 }
-
-class DeleteMemberController {
+class DeleteMemberController{
   constructor() {}
   DeleteMember(Member) {
     firebase.database().ref('user/' + userReference).remove();
     window.alert("Member Deleted");
     window.location.reload();
-  };
+  }
 }
 
 var db_cnx = new DatabaseConnection();
 var ui = new SlopeUI();
 
-
-  //  

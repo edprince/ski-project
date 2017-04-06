f101f729e8af4ae3c00456d20806348720953fb3
alasdairlincoln <lincoln3@uni.coventry.ac.uk>
2017-04-06 20:37:14 +0100

    slight modification to code structure

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -45,9 +45,31 @@ class controller { //controller class
 
     }//end of for loop
   }//end of constructor
+
+  function filter() {
+        // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+
 }//end of class
 
-class Singleton {//end of Singleton class, used to
+class Singleton {//end of Singleton class, used to insure only one instance of the controller
   constructor(){
     this.instance
   }
@@ -60,26 +82,4 @@ class Singleton {//end of Singleton class, used to
   }
 }
 
-
 Singleton.GetInstance();
-
-function myFunction() {
-      // Declare variables
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
